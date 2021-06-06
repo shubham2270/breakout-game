4a221b7dc8e553bdc4e4fe5177846e7b0cd54eeb
shubham2270 <shubham2270@gmail.com>
2021-06-06 17:00:54 +0530

    added paddle at bottom

--- script.js
+++ script.js
@@ -1,32 +1,46 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-const drawCircle = (x, y) => {
+// draws the ball
+const drawBall = () => {
   ctx.beginPath();
   ctx.arc(x, y, 15, 0, 2 * Math.PI, false);
-  ctx.fillStyle = "blue";
+  ctx.fillStyle = "#ff449f";
+  ctx.fill();
+  ctx.closePath();
+};
+
+// Draws the paddle
+const drawPaddle = () => {
+  ctx.beginPath();
+  ctx.rect(paddleX, paddleY, paddleW, 10);
+  ctx.fillStyle = "#00ead3";
   ctx.fill();
   ctx.closePath();
 };
 
 const detectCollision = () => {
+  // Detect if ball is crossing left or right side of box;
   if (x + dx >= canvas.width || x + dx < 0) {
     dx = -dx;
   }
 
-  if (y + dy > canvas.height || y + dy < 0) {
+  // Detect if ball is crossing ceiling of box;
+  if (y + dy < 0) {
     dy = -dy;
   }
 };
 
+// Set the starting position of ball & default distance it will travel
 const setVariables = () => {
   x = canvasH / 2;
-  y = canvasW - 20;
+  y = canvasW - 25;
 
   dx = 5; //small increment in pixels
   dy = -5;
 };
 
+// check if ball touches bottom of box & give alert of game over
 const checkGameOver = () => {
   if (y + dy === canvas.height) {
     alert("Game Over!");
@@ -35,16 +49,6 @@ const checkGameOver = () => {
     setVariables();
   }
 };
-
-const canvasH = canvas.height;
-const canvasW = canvas.width;
-
-let x, y, dy, dx;
-setVariables();
-
-drawCircle(x, y);
-
-let interval;
 
 const startGame = () => {
   interval = setInterval(() => {
@@ -55,8 +59,19 @@ const startGame = () => {
     checkGameOver();
 
     ctx.clearRect(0, 0, canvas.clientWidth, canvas.height);
-    drawCircle(x, y);
+    drawBall();
+    drawPaddle();
   }, 10);
 };
 
+const canvasH = canvas.height;
+const canvasW = canvas.width;
+
+let x, y, dy, dx, interval;
+let paddleW = 40;
+let paddleX = canvasW / 2 - paddleW / 2;
+let paddleY = canvasH - 10;
+setVariables();
+drawBall();
+drawPaddle();
 startGame();
